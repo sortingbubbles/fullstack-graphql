062ac50d0a16389bff818aa5b5bcaba462402313
Ani Chatsatrian <ani.chatsatrian@glovoapp.com>
2020-05-17 22:36:31 +0200

    feat: update schema with types, mutations and queries

--- api/src/schema.js
+++ api/src/schema.js
@@ -4,8 +4,37 @@ const { gql } = require('apollo-server')
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+  type User {
+    id: ID!
+    username: String!
+  }
 
+  type Pet {
+    id: ID!
+    createdAt: String
+    name: String!
+    type: String
+    owner: User
+  }
+
+  input PetInput {
+    name: String
+    id: ID
+  }
+
+  input NewPetInput {
+    name: String!
+    type: String!
+  }
+
+  type Mutation {
+    newPet(input: NewPetInput!): Pet!
+  }
+
+  type Query {
+    pets(input: PetInput): [Pet]!
+    pet(input: PetInput): Pet!
+  }
 `;
 
 module.exports = typeDefs
